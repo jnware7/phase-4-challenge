b46983a40e2eb8a903010a431adef6bd67f4449b
jnware7 <jnware7@gmail.com>
2017-08-02 10:03:39 -0700

    fixed seed data

---
+++ reviews.sql
@@ -0,0 +1,13 @@
+INSERT INTO
+  reviews (albums_id, review, users_id )
+ VALUES
+  ( 1,' Well, thats what you see at a toy store. And you must think youre in a toy store, because youre here shopping for an infant named Jeb.
+', 1),
+  ( 3, ' Well, thats what you see at a toy store. And you must think youre in a toy store, because youre here shopping for an infant named Jeb.
+', 1),
+  ( 2, ' Well, thats what you see at a toy store. And you must think youre in a toy store, because youre here shopping for an infant named Jeb.
+', 1),
+  ( 4, ' Well, thats what you see at a toy store. And you must think youre in a toy store, because youre here shopping for an infant named Jeb.
+', 1),
+  ( 1, ' Well, thats what you see at a toy store. And you must think youre in a toy store, because youre here shopping for an infant named Jeb.
+', 1)
--- src/public/script.js
+++ src/public/script.js
@@ -1,6 +1,6 @@
-// 
-// document.addEventListener('DOMContentLoaded', function () {
-//   document.querySelectorAll('.delete').forEach( button => button.addEventListener('click', function(event){
-//     confirm('Are you sure you would like to delete this Review?')
-//   }))
-// })
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelectorAll('.delete').forEach( button => button.addEventListener('click', function(event){
+    confirm('Are you sure you would like to delete this Review?')
+  }))
+})
